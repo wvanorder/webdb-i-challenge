5ffb0f9648dc18e71466b7330bb9a552b74aaf1e
VanOrder <vanorder@VanOrders-iMac.local>
2019-08-12 11:35:08 -0800

    mvp met and stretch achieved

---
+++ accounts/account-router.js
@@ -0,0 +1,84 @@
+const express = require('express');
+
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+
+//get all accounts, with some optional query string stuff
+router.get('/', (req, res) => {
+    // const query = db.select('name', 'budget').from('accounts');
+
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error while getting the account' });
+        })
+});
+
+//get a specific account
+router.get('/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(account => {
+            res.status(200).json(account);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error while getting the account' });
+        })
+});
+
+//create a new account
+router.post('/', (req, res) => {
+    const account = req.body;
+
+    db('accounts')
+        .insert(account, 'id')
+        .then(account => {
+            res.status(200).json({ message: `new account created. Account id is ${count}`});
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'internal server error on your wonderful creation' });
+        })
+});
+
+//delete a specific account
+router.delete('/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+        if(count > 0){
+            res.status(200).json({ message: 'Your account was deleted.'});
+        } else {
+            res.status(404).json({ message: 'could not find account to delete' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'internal server error on your wonderful deletion' });
+    })
+});
+
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    db('accounts')
+        .where({ id: req.params.id })
+        .update(changes)
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json({ message: 'your account was updated'})
+            } else {
+                res.status(404).json({ message: 'could not find account to update' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'internal server error on your wonderful updation (I know it isnt actually a real word but it flows with the others' });
+        })
+        
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const AccountsRouter = require('./accounts/account-router');
+
 
 const server = express();
 
 server.use(express.json());
+
+
+server.use('/api/accounts', AccountsRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h2>Time to get working on some code!</h2>')
+});
+
 
 module.exports = server;
